bdc7ffe58ce7ac2ac765044c7363ebfc2a99a9a6
SidiqiJaved <KJSidiqi@gmail.com>
2022-06-22 00:14:38 -0400

    second commit

--- src/Syntax/Main.java
+++ src/Syntax/Main.java
@@ -7,6 +7,11 @@ public class Main {
         for (int i=0; i<5; i++){
             System.out.println("printing i" );
         }
+int a=10;
+        int b=20;
 
+        if (a < b){
+            System.out.println("10 is less then 20" );
+        }
     }
 }
